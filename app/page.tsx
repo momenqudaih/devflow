b9d01a7f94e5d075f244cce54549751962d90e88
Momen Qudaih <momenqudaih1@gmail.com>
2024-11-16 16:03:41 +0200

    Add custom fonts and update global styles for improved typography

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,7 @@
-const Home = () => <h1 className="h1-bold">Welcome To Momen Site.</h1>;
-
+const Home = () => (
+  <>
+    <h1 className="h1-bold">Welcome To Momen Site.</h1>
+    <h1 className="h1-bold font-space-grotesk">Welcome To Momen Site.</h1>
+  </>
+);
 export default Home;
