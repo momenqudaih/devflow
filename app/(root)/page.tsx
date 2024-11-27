7d4ad00fc1ce2f7fdcf7d8b43bd3f70da937f085
Momen Qudaih <momenqudaih1@gmail.com>
2024-11-27 11:47:46 +0200

    Home Page Layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,12 +1,27 @@
-import { auth } from "@/auth";
+import Link from "next/link";
+
+import { Button } from "@/components/ui/button";
 
 const Home = async () => {
-  const session = await auth();
-
-  console.log(session);
   return (
     <>
-      <h1 className="h1-bold font-space-grotesk">Welcome To Momen Site.</h1>
+      <section className="flex flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
+        <h1 className="h1-bold text-dark100_light900">All Questions</h1>
+        <Button
+          className="primary-gradient min-h-[46px] px-4 py-3 !text-light-900"
+          asChild
+        >
+          <Link href="/login">Ask a Question</Link>
+        </Button>
+      </section>
+      <section className="mt-11">Local Search bar</section>
+      Filters Section
+      <div className="mt-10 flex w-full flex-col gap-6">
+        <p>Q Card</p>
+        <p>Q Card</p>
+        <p>Q Card</p>
+        <p>Q Card</p>
+      </div>
     </>
   );
 };
