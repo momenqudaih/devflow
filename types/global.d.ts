6b6c8bedde1467d109e2003d477d9ca7620b162e
Momen Qudaih <momenqudaih1@gmail.com>
2024-12-07 12:55:26 +0200

    Read & Create User api routes

--- types/global.d.ts
+++ types/global.d.ts
@@ -1,3 +1,5 @@
+import { NextResponse } from "next/server";
+
 interface Tag {
   _id: string;
   name: string;
@@ -20,3 +22,19 @@ interface Question {
   views: number;
   createdAt: Date;
 }
+
+type ActionResponse<T = null> = {
+  success: boolean;
+  data?: T;
+  error?: {
+    message: string;
+    details?: Record<string, string[]>;
+  };
+  status?: number;
+};
+
+type SuccessResponse<T = null> = ActionResponse<T> & { success: true };
+type ErrorResponse = ActionResponse<undefined> & { success: false };
+
+type ApiErrorResponse = NextResponse<ErrorResponse>;
+type ApiResponse<T = null> = NextResponse<SuccessResponse<T> | ErrorResponse>;
